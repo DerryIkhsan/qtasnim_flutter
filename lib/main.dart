f94d74f63291949f48a616f8903af9328150b13c
derry ikhsan sholahuddin <ikhsansholahuddin@gmail.com>
2022-12-29 02:52:20 +0700

    update Ui

--- lib/main.dart
+++ lib/main.dart
@@ -47,6 +47,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF0F3F5),
       body: SafeArea(
         child: Container(
           margin: EdgeInsets.all(30),
--- lib/screen/transaksi/transaksi.dart
+++ lib/screen/transaksi/transaksi.dart
@@ -29,53 +29,111 @@ class _TransaksiState extends State<Transaksi> {
             itemCount: snapshot.data?.length,
             itemBuilder: (context, index) {
               return Container(
-                margin: EdgeInsets.all(20),
-                height: 200,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 5, bottom: 15),
+                height: 160,
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(20),
-                ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 1,
+                        color: Colors.black.withOpacity(.6),
+                      )
+                    ]),
                 child: Container(
-                  margin: EdgeInsets.only(left: 30, right: 30, top: 30),
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 20),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
                           Text(
                             "${snapshot.data?[index]['tgl_transaksi']}",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.black54,
+                                fontSize: 14),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.local_mall,
+                            size: 70,
+                            color: Colors.green,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                "${snapshot.data?[index]['nama_barang']}",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 20),
+                              ),
+                              SizedBox(
+                                height: 3,
+                              ),
+                              Text(
+                                "${snapshot.data?[index]['jumlah']} item",
+                              ),
+                            ],
                           ),
                         ],
                       ),
                       SizedBox(
-                        height: 15,
+                        height: 20,
                       ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text(
-                            "${snapshot.data?[index]['nama_barang']}",
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.beenhere,
+                                size: 20,
+                                color: Colors.red,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "${snapshot.data?[index]['jenis_barang']}",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.black54,
+                                    fontSize: 14),
+                              ),
+                            ],
                           ),
-                          SizedBox(
-                            width: 15,
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.check_circle,
+                                size: 20,
+                                color: Colors.green,
+                              ),
+                              SizedBox(width: 5),
+                              Text(
+                                "Tersedia ${snapshot.data?[index]['stok_awal']}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  color: Colors.black54,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
                           ),
-                          Text(
-                            "Stok: ${snapshot.data?[index]['stok_awal']}",
-                          ),
-                          SizedBox(
-                            width: 15,
-                          ),
-                          Text(
-                            "Jumlah Terjual: ${snapshot.data?[index]['jumlah']}",
-                          ),
-                          // SizedBox(
-                          //   width: 15,
-                          // ),
-                          // Text(
-                          //   "Jenis: ${snapshot.data?[index]['jenis_barang']}",
-                          // ),
                         ],
-                      ),
+                      )
                     ],
                   ),
                 ),
